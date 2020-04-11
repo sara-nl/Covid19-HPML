02662dfe501994f8f78005eb6262ba874231f81b
Damian <damian@surfsara.nl>
2020-04-11 22:08:53 +0200

    Fixed some of the configuration files

--- XRAY/automlpytorch/autoimage.py
+++ XRAY/automlpytorch/autoimage.py
@@ -12,14 +12,18 @@ from sklearn import preprocessing
 
 input_size = (256, 256)
 channels = 3
+final_dataset_location = "/tmp/covid_dataset"
+initial_dataset_location = "../data"
+save_output_to = "3class_280balanced_ba_ce"
 classes = ["covid", "Pneumonia", "No Finding"]
 
-def get_data_references(images = [] , labels = [] , filter = "_positive.txt", copy_to = "/tmp/covid_dataset", balance = 280, classes=classes, img_channels = channels): #TODO: add balanced/imbalanced and number of examples limit 
+
+def get_data_references(images = [] , labels = [] , filter = "_positive.txt", copy_to = final_dataset_location, copy_from = initial_dataset_location, balance = 280, classes=classes, img_channels = channels):
     '''
     If you don't  want to copy set copy_to = None; only if this is set img_channels is taken into account 
     balance = 180 means each class gets 180 examples before splits
     '''
-    for name in glob.glob("../data/*{}".format(filter)):
+    for name in glob.glob(initial_dataset_location + os.path.sep + "*{}".format(filter)):
         class_name = name.split(filter)[0].split("/")[-1].lower()
         if any(class_name in s.lower() for s in classes):
             for i, imagepaths in enumerate(open(name).readlines()):
@@ -67,31 +71,31 @@ if __name__ == "__main__":
     #Get the dataset (this could be yielded/batched)
     images, labels, le  = get_data_references()
     search_space_updates = HyperparameterSearchSpaceUpdates()
-    search_space_updates.append(node_name="CreateImageDataLoader",
-                                hyperparameter="batch_size",
-                                value_range=[2, int(len(labels)/20)], #Lipschitz magical number
-                                log=False)
+    #TODO: this still runs out of memory and wastes resources
+    search_space_updates.append(node_name="CreateImageDataLoader", hyperparameter="batch_size", log=False, \
+                                value_range=[2, int(len(labels)/20)]) #Lipschitz magical number
     autoPyTorch = AutoNetImageClassification(
-                                        "full_cs",
-                                        hyperparameter_search_space_updates=search_space_updates,
-                                        min_workers=2,
-                                        dataloader_worker=4,
-                                        global_results_dir="results",
-                                        keep_only_incumbent_checkpoints=True, 
-                                        log_level="info",
-                                        budget_type="epochs",
-                                        save_checkpoints=True,
-                                        result_logger_dir="logs",
-                                        min_budget=1,
-                                        max_budget=20,
-                                        num_iterations=5, 
-                                        images_shape=[channels, input_size[0], input_size[1]],
-                                        cuda=True)
-    #Fit
+                                        "full_cs", \
+                                        hyperparameter_search_space_updates=search_space_updates, \
+                                        min_workers=2, \
+                                        dataloader_worker=4, \
+                                        global_results_dir="results", \
+                                        keep_only_incumbent_checkpoints=True, \
+                                        log_level="info", \
+                                        budget_type="epochs", \
+                                        save_checkpoints=True, \
+                                        result_logger_dir=save_output_to, \
+                                        min_budget=1, \
+                                        max_budget=60, \
+                                        num_iterations=5, \
+                                        images_shape=[channels, input_size[0], input_size[1]], \
+                                        cuda=True \
+                                        )
+    #fit
+    # autoPyTorch.fit(images, labels, use_tensorboard_logger=True, validation_split=0.1)
     # autoPyTorch.fit(images, labels, optimize_metric="balanced_accuracy", use_tensorboard_logger=True, loss_modules=['cross_entropy'], validation_split=0.1)
     # autoPyTorch.fit(images, labels, optimize_metric="accuracy", use_tensorboard_logger=True, networks=['resnet'], lr_scheduler=['cosine_annealing'], batch_loss_computation_techniques=['mixup'], loss_modules=['cross_entropy'], validation_split=0.1)
     autoPyTorch.fit(images, labels, optimize_metric="balanced_accuracy",  networks=['densenet', 'densenet_flexible', 'resnet', 'resnet152', 'darts'], use_tensorboard_logger=True, loss_modules=["cross_entropy"], validation_split=0.1)
-    #Print
     print("autoPyTorch.get_autonet_config_file_parser().print_help()")
     print(autoPyTorch.get_autonet_config_file_parser().print_help())
     print("autoPyTorch.get_hyperparameter_search_space()")
